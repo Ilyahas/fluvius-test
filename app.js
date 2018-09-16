1dd0158159db2c146bf79ae21626a8e6be3bd5dd
IlyaKot <info-site@ukr.net>
2018-09-16 19:29:02 +0300

    authorization

--- app.js
+++ app.js
@@ -6,5 +6,4 @@ require('./config/express')(app);
 require('./config/mongoose')();
 require('./config/routes')(app);
 
-
 app.listen(config.PORT, '0.0.0.0', () => console.log("App listening on port " + config.PORT + "!"));
